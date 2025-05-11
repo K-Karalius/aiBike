41c082c7b38cb0c553c0c78764b8e9fd4891bbb0
rokas-migauskas <37188343+rokas-migauskas@users.noreply.github.com>
2025-05-11 19:23:38 +0300

    improvements

---
+++ server/Features/Stations/GetAll/GetAllStationsResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace server.Features.Stations.GetAll;
+
+public record GetAllStationsResponse
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public decimal Latitude { get; set; }
+    public decimal Longitude { get; set; }
+    public int Capacity { get; set; }
+    public int BikeCount { get; set; }
+};

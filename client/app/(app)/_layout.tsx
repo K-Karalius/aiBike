691dd212f78e327085fa77505ca7f5d02632e329
MartynasJonkus <martynez00@gmail.com>
2025-05-15 16:41:25 +0300

    Updated map, removed header

--- client/app/(app)/_layout.tsx
+++ client/app/(app)/_layout.tsx
@@ -10,6 +10,7 @@ export default function AppLayout() {
   return (
     <Tabs
       screenOptions={{
+        headerShown: false,
         tabBarActiveTintColor: '#007bff',
         tabBarInactiveTintColor: 'gray',
         tabBarStyle: {

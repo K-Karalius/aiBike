2589600ca26af7d93262e361198bfa266985b316
MartynasJonkus <martynez00@gmail.com>
2025-05-20 02:50:53 +0300

    updated radius to be larger

--- server/Features/BusinessConstants/GetBusinessConstants.cs
+++ server/Features/BusinessConstants/GetBusinessConstants.cs
@@ -6,7 +6,7 @@ namespace server.Features.BusinessConstants.Get;
 public class GetBusinessConstants : IEndpoint
 {
     public const decimal farePerMinute = 3;
-    public const decimal stationRadius = 5;
+    public const decimal stationRadius = 50;
 
     public RouteHandlerBuilder MapEndpoint(IEndpointRouteBuilder builder) =>
         builder.MapGet("/api/constants",
